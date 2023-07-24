c0e430e6bcc43f817419c9f5a18c993b21fbc192
Luan Braga <126211492+luanbg12@users.noreply.github.com>
2023-07-24 19:22:19 -0300

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,13 @@
+let x = 1
+
+while (x<=3) {
+    alert('valor x:' + x)
+    x++
+}
+
+alert ('Fim Loop')
+///FOR
+const valor = Number(prompt('digite o valor'))
+for (let i =1: i <=10; i++){
+alert(i + 'x' + valor +'=' + i * valor)
+}
