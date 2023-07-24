d78073a146295da8c5736b7f2146698b8bc4896d
Luan Braga <126211492+luanbg12@users.noreply.github.com>
2023-07-24 19:20:29 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+let Number = parseFloat(prompt('número 1'))
+let Number2 = parseFloat(prompt('número 2'))
+
+switch (menu) {
+    case 1:
+        let Adicionar = parseFloat(prompt('Adicionar'))
+        Adicionar = Number2 + Adicionar
+        alert('Seu resultado' + Adicionar)
+        break
+case 2:
+    let Diminuir = parseFloat(prompt('Diminuir'))
+    Diminuir = Number2 - Diminuir
+    alert = ('Seu resultado:' + Diminuir)
+    break
+case 3:
+    let Multiplicação = parseFloat(prompt('multiplique'))
+    Multiplicação = Number2 * Multiplicação
+    alert('Seu resultado:' + Multiplicação)
+    break
+    case 4:
+        let divisão = parseFloat(prompt ('divisão'))
+        Divisão = Number2 / divisão
+        alert ('Seu resultado:' + divisão)
+}
