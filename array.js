a19bc069978f91138d63ee9165aadc682c85e989
Luan Braga <126211492+luanbg12@users.noreply.github.com>
2023-07-24 19:18:56 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,13 @@
+const arr = ["Frodo","Sam","Merry","Pippin","Gandalf","Aragorn","legolas","Gimli"]
+
+//let tamanho = arr.push('Boromir')
+//console.log(arr)
+//console.log(tamanho)
+
+let tamanho = arr.unshift("Boromir")
+console.log(arr)
+console.log(tamanho)
+
+let tamanho = arr.pop('Gimli')
+console.log(arr)
+console.log(tamanho)
