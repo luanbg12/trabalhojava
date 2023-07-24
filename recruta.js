8ff4ae1498bc44d76e23da4589b0828a804ce19b
Luan Braga <126211492+luanbg12@users.noreply.github.com>
2023-07-24 19:22:46 -0300

    Create recruta.js

---
+++ recruta.js
@@ -0,0 +1,8 @@
+const primeiroNome = prompt('Digite o primeiro nome:')
+const segundoNome = prompt ('Digite o segundo nome:')
+const campoEstudo = prompt ('Digite o campo de Estudo:')
+const anoNascimento = prompt ('Digite o ano de Nascimento:')
+
+alert ('Nome Completo:'+primeiroNome+' '+segundoNome+
+'\nCampo de Estudo:'+campoEstudo+
+'\nIdade:'+anoNascimento)
